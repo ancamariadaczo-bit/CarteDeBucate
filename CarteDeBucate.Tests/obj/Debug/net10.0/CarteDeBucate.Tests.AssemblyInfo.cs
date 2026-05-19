9b4d3f46f765ede6f2a32f21cdc6c317f8a434ec
ancamariadaczo-bit <ancamariadaczo@gmail.com>
2026-05-19 10:38:06 +0300

    Added UpdateRecipe method in repos

--- CarteDeBucate.Tests/obj/Debug/net10.0/CarteDeBucate.Tests.AssemblyInfo.cs
+++ CarteDeBucate.Tests/obj/Debug/net10.0/CarteDeBucate.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarteDeBucate.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd88c851be1d6c84b5d732c8c91d94f97cddfbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40923d97b5ac64e91ae03bd69b84b28415ad8145")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarteDeBucate.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarteDeBucate.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
