8cc789044ab8ea0f82b57745020ee6aed18c604d
ancamariadaczo-bit <ancamariadaczo@gmail.com>
2026-06-08 09:40:04 +0300

    Commit all files that were left behind.

--- CarteDeBucate.Tests/obj/Debug/net10.0/CarteDeBucate.Tests.AssemblyInfo.cs
+++ CarteDeBucate.Tests/obj/Debug/net10.0/CarteDeBucate.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarteDeBucate.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1570e798077ec0f51bab9fc0df9f80c5a9074448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a553135ac63769860cb6bfd4043d877bd1e2d410")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarteDeBucate.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarteDeBucate.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
