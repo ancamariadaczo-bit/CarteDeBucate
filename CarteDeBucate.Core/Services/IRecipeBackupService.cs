3fc3a800346d925b41a4ff9b9235d1b8f7676f1a
ancamariadaczo-bit <ancamariadaczo@gmail.com>
2026-07-07 11:13:51 +0300

    Structural changes in Core

--- CarteDeBucate.Core/Services/IRecipeBackupService.cs
+++
@@ -1,7 +0,0 @@
-public interface IRecipeBackupService
-{
-    RecipeBackupResult ExportToJson(string backupFilePath);
-    RecipeBackupExportResult ExportToJsonContent();
-    RecipeBackupResult ImportFromJson(string backupFilePath);
-    RecipeBackupResult ImportFromJsonContent(string json);
-}
